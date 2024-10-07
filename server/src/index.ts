9d8591a05af52a0e0779ae626978a5e6f09647c9
syahrilr <syahrilramadhan754@gmail.com>
2024-10-07 22:07:01 +0800

    server modified

--- server/src/index.ts
+++ server/src/index.ts
@@ -33,8 +33,8 @@ app.use("/users", userRoutes)
 app.use("/teams", teamRoutes)
 
 // Server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server is running on port ${port}`);
     
 })
